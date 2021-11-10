dc31b22b37d95c2ff77a3e8d1ea15d301959b62e
kelcheone <kelcheone@gmail.com>
2021-11-10 10:05:21 +0300

    view engines

--- server1/app.js
+++ server1/app.js
@@ -4,20 +4,34 @@ const port = 5000;
 
 app.set('view engine', 'ejs')
 app.get("/", (req, res) => {
-  res.render('index')
+  const blogs = [
+    {
+      title: "The definition of life",
+      snippets: "The definition of life is 42",
+    },
+    {
+      title: "Alpha is Mega",
+      snippets: "Why is this phenomenon universally true to us all? ",
+    },
+    {
+      title: "Health is not wealth",
+      snippets: "The real meaning is that health is more than wealth",
+    },
+  ]
+  res.render('index', {title: 'Home', blogs})
 });
 app.get("/about", (req, res) => {
-  res.render('about')
+  res.render('about', {title: "About", })
 });
 app.get("/about-me", (req, res) => {
   res.redirect("/about");
 });
 
 app.get('/blogs/create', (req, res)=>{
-  res.render('create')
+  res.render('create', {title: 'Create', })
 })
 
 app.use((req, res) => {
-  res.render('404')
+  res.render('404', {title: '404'})
 });
 app.listen(port);
